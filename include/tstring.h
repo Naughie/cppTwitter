f278eaadd823cbfe9638f8367f41f36f79f4803f
Naughie <zomacchi@gmail.com>
2018-01-31 05:37:20 +0900

    Add methods to push chars

--- include/tstring.h
+++ include/tstring.h
@@ -19,17 +19,36 @@ class string
     ~string();
     char *begin();
     char *end();
+    size_t size();
     template<typename T>
     string &operator<<(T *str);
     template<typename T>
     string &operator<<(T &str);
     string &operator<<(char c);
+    string &operator<<(int n);
+    string &operator<<(unsigned int n);
+    string &operator<<(long n);
+    string &operator<<(unsigned long n);
+    template<typename T>
+    static void encode(EncodeMethod enc, T *from, string &to);
+    template<typename T>
+    static void encode(EncodeMethod enc, T &from, string &to);
   private:
     char *body;
-    size_t size;
-    void push_back(char c);
+    size_t len;
     void push_back(char *str);
     void push_back(const char *str);
+    void push_back(string &str);
+    template<typename T>
+    void push_back(T n);
+    template<typename T>
+    static void url_encode(T *from, string &to);
+    template<typename T>
+    static void url_encode(T &from, string &to);
+    template<typename T>
+    static void base64_encode(T *from, string &to);
+    template<typename T>
+    static void base64_encode(T &from, string &to);
 };
 
 }
--- src/tstring.cpp
+++ src/tstring.cpp
@@ -5,12 +5,12 @@ namespace twitter
 {
 
 string::string()
-  : body(new char[BUFSIZE]), size(0)
+  : body(new char[BUFSIZE]), len(0)
 {
 }
 
 string::string(unsigned int buf_size)
-  : body(new char[buf_size]), size(0)
+  : body(new char[buf_size]), len(0)
 {
 }
 
@@ -19,14 +19,19 @@ string::~string()
   delete [] body;
 }
 
-char *string::begin()
+inline char *string::begin()
 {
   return body;
 }
 
-char *string::end()
+inline char *string::end()
 {
-  return body + size;
+  return body + size();
+}
+
+inline size_t string::size()
+{
+  return len;
 }
 
 template<typename T>
@@ -46,29 +51,118 @@ string &string::operator<<(T &str)
   return *this;
 }
 
+template string &string::operator<<(string &str);
+
 string &string::operator<<(char c)
 {
-  push_back(c);
+  *(body + len++) = c;
   return *this;
 }
 
-void string::push_back(char c)
+string &string::operator<<(int n)
 {
-  *(body + size++) = c;
+  push_back<int>(n);
+  return *this;
+}
+
+string &string::operator<<(unsigned int n)
+{
+  push_back<unsigned int>(n);
+  return *this;
+}
+
+string &string::operator<<(long n)
+{
+  push_back<long>(n);
+  return *this;
+}
+
+string &string::operator<<(unsigned long n)
+{
+  push_back<unsigned long>(n);
+  return *this;
+}
+
+template<typename T>
+void string::encode(EncodeMethod enc, T *from, string &to)
+{
+  switch (enc) {
+    case URL_ENCODE:
+      url_encode(from, to);
+      break;
+    case BASE64_ENCODE:
+      base64_encode(from, to);
+      break;
+    default:
+      break;
+  }
+}
+
+template<typename T>
+void string::encode(EncodeMethod enc, T &from, string &to)
+{
+  switch (enc) {
+    case URL_ENCODE:
+      url_encode(from, to);
+      break;
+    case BASE64_ENCODE:
+      base64_encode(from, to);
+      break;
+    default:
+      break;
+  }
 }
 
 void string::push_back(char *str)
 {
-  int len = strlen(str);
-  memcpy(body + size, str, len);
-  size += len;
+  int size = strlen(str);
+  memcpy(end(), str, size);
+  len += size;
 }
 
 void string::push_back(const char *str)
 {
-  int len = strlen(str);
-  memcpy(body + size, str, len);
-  size += len;
+  int size = strlen(str);
+  memcpy(end(), str, size);
+  len += size;
+}
+
+void string::push_back(string &str)
+{
+  memcpy(end(), str.begin(), str.size());
+  len += str.size();
+}
+
+template<typename T>
+void string::push_back(T n)
+{
+  char m[20];
+  m[19] = 0x30 + n % 10;
+  int i = 19;
+  while (n /= 10)
+    m[--i] = 0x30 + n % 10;
+  memcpy(end(), m + i, 20 - i);
+  len += 20 - i;
+}
+
+template<typename T>
+void string::url_encode(T *from, string &to)
+{
+}
+
+template<typename T>
+void string::url_encode(T &from, string &to)
+{
+}
+
+template<typename T>
+void string::base64_encode(T *from, string &to)
+{
+}
+
+template<typename T>
+void string::base64_encode(T &from, string &to)
+{
 }
 
 }
